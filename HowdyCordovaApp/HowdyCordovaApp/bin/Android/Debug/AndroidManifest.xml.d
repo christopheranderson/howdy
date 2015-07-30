96a190e57ac3b8017a7e6227bd75d01a0a1bcb91
Chris Anderson <chrande@outlook.com>
2015-07-30 01:58:53 -0700

    [Update] Fixed up Cordova App

---
+++ HowdyCordovaApp/HowdyCordovaApp/bin/Android/Debug/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+Z:\workspace\chrande-howdy\HowdyCordovaApp\HowdyCordovaApp\platforms\android\ant-build\AndroidManifest.xml : \
+Z:\workspace\chrande-howdy\HowdyCordovaApp\HowdyCordovaApp\platforms\android\AndroidManifest.xml \
---
+++ HowdyCordovaApp/HowdyCordovaApp/bin/Android/Debug/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+Z:\workspace\chrande-howdy\HowdyCordovaApp\HowdyCordovaApp\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
+Z:\workspace\chrande-howdy\HowdyCordovaApp\HowdyCordovaApp\platforms\android\ant-build\MainActivity.ap_ \
+Z:\workspace\chrande-howdy\HowdyCordovaApp\HowdyCordovaApp\platforms\android\ant-build\classes.dex \
+Z:\workspace\chrande-howdy\HowdyCordovaApp\HowdyCordovaApp\platforms\android\CordovaLib\ant-build\classes.jar \
